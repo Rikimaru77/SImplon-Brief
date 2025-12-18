cf88b9e6de6535b2e749070d3dde2792233b9da4
Kokouette <mateo0650079@gmail.com[D>
2025-12-18 13:32:16 +0100

    Add script.js and made the login register button worked, register and login form are now added but not functionning

---
+++ script.js
@@ -0,0 +1,31 @@
+const Registerbtn = document.getElementById("RegisterButtonA");
+const Registerbtn1 = document.getElementById("RegisterButtonB");
+const RegisterForm = document.getElementById("RegisterForm");
+const WelcomeUser = document.getElementById("WelcomePage");
+const LogInbtn = document.getElementById("LogInButtonA");
+const LogInbtn1 = document.getElementById("LogInButtonB");
+const LogInForm = document.getElementById("LogInForm");
+
+Registerbtn.addEventListener("click",()=> {
+    RegisterForm.style.display = "flex";
+    WelcomeUser.style.display = "none";
+    LogInForm.style.display = "none";
+});
+
+LogInbtn.addEventListener("click", ()=> {
+    LogInForm.style.display = "flex";
+    RegisterForm.style.display = "none"
+    WelcomeUser.style.display = "none";
+})
+
+Registerbtn1.addEventListener("click",()=> {
+    RegisterForm.style.display = "flex";
+    WelcomeUser.style.display = "none";
+    LogInForm.style.display = "none";
+});
+
+LogInbtn1.addEventListener("click", ()=> {
+    LogInForm.style.display = "flex";
+    RegisterForm.style.display = "none"
+    WelcomeUser.style.display = "none";
+})
